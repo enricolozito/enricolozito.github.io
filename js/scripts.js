7b03ed6d2b0f01921a11ede6e1e01a9cb30bd593
enricolozito <enrico.lozito@gmail.com>
2014-04-19 14:16:20 +0200

    responsive top menu

--- js/scripts.js
+++ js/scripts.js
@@ -10,18 +10,29 @@ $(document).ready(function(){
     $("#artista").click(function() {
         $("#supersized").remove();
         $("#arrows").remove();    
-        $( "section#target" ).load( 'artista.html' ).hide().fadeIn(3000);     
+        $( "section#target" ).load( 'artista.html' ).hide().fadeIn(3000); 
+        $("#artista").addClass("current"); 
+        $("#galeria").removeClass("current"); 
+        $("#contacto").removeClass("current");         
         });
     
     $("#galeria").click(function() {
         $("#supersized").remove();
         $("#arrows").remove();    
-        $( "section#target" ).load( 'galeria.html' ).hide().fadeIn(3000);        
+        $( "section#target" ).load( 'galeria.html' ).hide().fadeIn(3000);  
+        $("#galeria").addClass("current");   
+        $("#artista").removeClass("current"); 
+        $("#contacto").removeClass("current");         
         });
     
     $("#contacto").click(function() {
         $("#supersized").remove();
         $("#arrows").remove();    
-        $( "section#target" ).load( 'contacto.html' ).hide().fadeIn(3000);       
-        });    
+        $( "section#target" ).load( 'contacto.html' ).hide().fadeIn(3000);  
+        $("#contacto").addClass("current"); 
+        $("#artista").removeClass("current"); 
+        $("#galeria").removeClass("current");          
+        }); 
+    
+
 });
